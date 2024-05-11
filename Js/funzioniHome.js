aa5bad6988339765f5336438abe74278a212e62d
Arsanios Rezk <114307435+ArsaniosRezk@users.noreply.github.com>
2024-05-11 12:13:45 +0200

    memorizzazione divisione

--- Js/funzioniHome.js
+++ Js/funzioniHome.js
@@ -35,38 +35,9 @@ import {
 
 const db = getDatabase();
 
-const divisioneSelect = document.getElementById("divisione");
-const divisioneSelectSmartphone = document.getElementById(
-  "divisione-smartphone"
-);
-let selectedDivisione = "Superiori";
-
-// Funzioni che partono al caricamento della pagina
-document.addEventListener("DOMContentLoaded", () => {
-  sequenzaEsecuzione();
-});
-
-// Funzioni che partono quando cambio divisione
-divisioneSelect.addEventListener("change", async () => {
-  selectedDivisione = divisioneSelect.value;
-
-  // Chiamata alla funzione per visualizzare le squadre
-  sequenzaEsecuzione();
-});
-
-// Funzioni che partono quando cambio divisione
-divisioneSelectSmartphone.addEventListener("change", async () => {
-  selectedDivisione = divisioneSelectSmartphone.value;
-
-  // Chiamata alla funzione per visualizzare le squadre
-  sequenzaEsecuzione();
-});
-
 // Chiamata alla funzione principale con gestione della sequenza
-async function sequenzaEsecuzione() {
+export async function sequenzaEsecuzione() {
   recuperaProssimaGiornata();
-
-  // visualizzaSquadreHome();
   calcolaClassificaGirone();
 }
 
