fc6c2b1343c9d4d0f97cf817f3dcb503c103ffeb
Arsanios Rezk <114307435+ArsaniosRezk@users.noreply.github.com>
2024-10-14 15:52:14 +0200

    Update foto-e-video.js

--- Js/foto-e-video.js
+++ Js/foto-e-video.js
@@ -70,14 +70,14 @@ const images = [
 
 // Lista dei video (puoi espandere questa lista con tutti i tuoi file video)
 const videos = [
-  { src: "Video/giornata 1.mp4", poster: "asset/Copertina-Video.jpg" },
-  { src: "Video/giornata 2.mp4", poster: "asset/Copertina-Video.jpg" },
-  { src: "Video/giornata 3.mp4", poster: "asset/Copertina-Video.jpg" },
-  { src: "Video/giornata 4.mp4", poster: "asset/Copertina-Video.jpg" },
-  { src: "Video/giornata 5.mp4", poster: "asset/Copertina-Video.jpg" },
-  { src: "Video/semifinali.mp4", poster: "asset/Copertina-Video.jpg" },
-  // { src: "Video/finale-superiori.mp4", poster: "asset/Copertina-Video.jpg" },
-  // { src: "Video/finale-giovani.mp4", poster: "asset/Copertina-Video.jpg" },
+  { src: "Video/giornata 1.mp4", poster: "Asset/Copertina-Video.jpg" },
+  { src: "Video/giornata 2.mp4", poster: "Asset/Copertina-Video.jpg" },
+  { src: "Video/giornata 3.mp4", poster: "Asset/Copertina-Video.jpg" },
+  { src: "Video/giornata 4.mp4", poster: "Asset/Copertina-Video.jpg" },
+  { src: "Video/giornata 5.mp4", poster: "Asset/Copertina-Video.jpg" },
+  { src: "Video/semifinali.mp4", poster: "Asset/Copertina-Video.jpg" },
+  { src: "Video/finale-superiori.mp4", poster: "asset/Copertina-Video.jpg" },
+  { src: "Video/finale-giovani.mp4", poster: "asset/Copertina-Video.jpg" },
 
   // Aggiungi altri video
 ];
