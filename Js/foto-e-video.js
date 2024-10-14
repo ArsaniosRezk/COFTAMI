4b3331d01cce0584dcce0e0b881228028cd8f1de
Arsanios Rezk <114307435+ArsaniosRezk@users.noreply.github.com>
2024-10-14 16:07:25 +0200

    Update foto-e-video.js

--- Js/foto-e-video.js
+++ Js/foto-e-video.js
@@ -76,7 +76,7 @@ const videos = [
   { src: "Video/giornata 4.mp4", poster: "Asset/Copertina-Video.jpg" },
   { src: "Video/giornata 5.mp4", poster: "Asset/Copertina-Video.jpg" },
   { src: "Video/semifinali.mp4", poster: "Asset/Copertina-Video.jpg" },
-  { src: "Video/finale-superiori.mp4", poster: "Asset/Copertina-Video.jpg" },
+  { src: "Video/finale-superiori.MP4", poster: "Asset/Copertina-Video.jpg" },
   { src: "Video/finale-giovani.MP4", poster: "Asset/Copertina-Video.jpg" },
 
   // Aggiungi altri video
